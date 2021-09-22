a206cb933af48217c41a2ef4e0e06648237621d2
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-22 19:07:37 +0000

    make moves for snake input, refactor codes

--- input.js
+++ input.js
@@ -1,26 +1,43 @@
+let connection;
+
+// const { stdin } = require('process');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', handleUserInput);
+  stdin.on('data', (key) => { handleUserInput(key) });
 
   return stdin;
 }
 
-const handleUserInput = function (data) {
-  if (data.key === 'c' && data.key.name === key.ctrl) {
+const handleUserInput = function (key) {
+  if (key === 'c' &&
+    key.name === key.ctrl) {
     rl.close();
   }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
+  }
+
 }
 
-module.exports = {
-  setupInput
-}
+module.exports = setupInput;
