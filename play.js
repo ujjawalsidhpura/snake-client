1629082f9c60b1adc1691e0d01428527f14198dd
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-22 16:54:11 +0000

    connecting server logic moved into client file

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require('net');
+const { connect } = require('./client');
 
-//establish connection with a game server
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542'
-  });
-
-  // interpret incoming data as Text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('Incoming data -->', data)
-  })
-
-  return conn;
-}
 
 console.log('Connecting...');
 connect();
